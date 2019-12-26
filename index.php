e51c240c74078f2c1730772680daebeb11dc3d08
victor.medeiros <victorbraga.8@hotmail.com>
2019-12-26 12:58:36 -0300

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- EM CASO DE USO DO BOOTSTRAP, FAÇA A CHAMADA VIA CDN OU APONTAMENTO DE SRC DOS ARQUIVOS USADOS, A ESTRUTURA DO TEMA NÃO APRESENTA RELAÇÃO OU VINCULO QUE GERE INCOMPATIBILIDADE  -->
+
+<!-- ARQUIVOS SLIDER REVOLUTION HEADER -->	  
+	<link rel="stylesheet" href="vendor/rs-plugin/css/settings.css">
+	<link rel="stylesheet" href="vendor/rs-plugin/css/theme.css">        
+<!--FIM ARQUIVOS SLIDER REVOLUTION HEADER -->
+
+ <!-- ARQUIVOS SLIDER REVOLUTION FOOTER-->	  
+	<link rel="stylesheet" href="vendor/rs-plugin/css/settings.css">
+	<link rel="stylesheet" href="vendor/rs-plugin/css/theme.css">        
+ <!--FIM ARQUIVOS SLIDER REVOLUTION FOOTER-->
